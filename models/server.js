5818c2e19a573d688dfdc60f541781e9d442f56f
Juan Alesandri <juan.alesandri.acc@gmail.com>
2023-05-23 11:36:30 -0300

    Creacion proyecto para desplegar en Render

--- models/server.js
+++ models/server.js
@@ -5,7 +5,7 @@ const {dbConnection} = require('../database/config');
 class Server {
     constructor() {
         this.app = express();
-        this.port = process.env.PORT;
+        this.port = process.env.PORT || 3000;
 
         this.authPath = '/api/auth';
         this.recipesPath = '/api/recipes';
